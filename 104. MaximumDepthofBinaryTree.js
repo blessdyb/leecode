ce937221d98fea60e7c663b3a38022af53f0042e
blessdyb <blessdyb@gmail.com>
2021-03-16 15:34:51 -0700

    Update 104. MaximumDepthofBinaryTree.js

--- 104. MaximumDepthofBinaryTree.js
+++ 104. MaximumDepthofBinaryTree.js
@@ -16,3 +16,28 @@ var maxDepth = function(root) {
     }
     return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
 };
+
+var maxDepth = function(root) {
+    let depth = 0;
+    if (root) {
+        const stack = [root];
+        let count = stack.length;
+        let cursor = 0;
+        while(stack.length) {
+            const node = stack.shift();
+            cursor++;
+            if (node.left) {
+                stack.push(node.left);
+            }
+            if (node.right) {
+                stack.push(node.right);
+            }
+            if (cursor === count) {
+                depth++;
+                count = stack.length;
+                cursor = 0;
+            }
+        }
+    }
+    return depth;
+}
