d9bdb4361bfaf5313368781fb3ba8ffe9bcc9b3d
blessdyb <blessdyb@gmail.com>
2021-03-17 14:41:33 -0700

    Update 235.LowestCommonAncestorofaBinarySearchTree.js

--- 235.LowestCommonAncestorofaBinarySearchTree.js
+++ 235.LowestCommonAncestorofaBinarySearchTree.js
@@ -26,3 +26,22 @@ var lowestCommonAncestor = function(root, p, q) {
         return root;
     }
 };
+
+var lowestCommonAncestor = function(root, p, q) {
+    const stack = [root];
+    const low = p.val > q.val ? q : p;
+    const high = p.val > q.val ? p : q;
+    while(stack.length) {
+        const node = stack.shift();
+        if (node.val === low.val || node.val === high.val) {
+            return node;
+        }
+        if (node.val < low.val) {
+            stack.push(node.right);
+        } else if (node.val > high.val) {
+            stack.push(node.left);
+        } else {
+            return node;
+        }
+    }
+};
