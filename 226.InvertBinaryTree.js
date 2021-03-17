758ad1815b5ba5ae1825e3a0c736419b56b976f1
blessdyb <blessdyb@gmail.com>
2021-03-17 14:06:44 -0700

    Update 226.InvertBinaryTree.js

--- 226.InvertBinaryTree.js
+++ 226.InvertBinaryTree.js
@@ -18,3 +18,18 @@ var invertTree = function(root) {
     }
     return root;
 };
+
+var invertTree = function(root) {
+    const stack = [root];
+    while(stack.length) {
+        const node = stack.pop();
+        if (node) {
+            const temp = node.left;
+            node.left = node.right;
+            node.right = temp;
+            stack.push(node.left);
+            stack.push(node.right);
+        }
+    }
+    return root;
+};
