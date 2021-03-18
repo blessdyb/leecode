ff8909a3e59f4219e6d2ca4e540b6bd9b67cdbfe
blessdyb <blessdyb@gmail.com>
2021-03-17 23:16:47 -0700

    Update 501.FindModeinBinarySearchTree.js

--- 501.FindModeinBinarySearchTree.js
+++ 501.FindModeinBinarySearchTree.js
@@ -11,5 +11,26 @@
  * @return {number[]}
  */
 var findMode = function(root) {
-    
+    let result = [];
+    let current = 0;
+    let count = 0;
+    let maxCount = 0;
+    (function dfs(node) {
+        if (node) {
+            dfs(node.left);
+            if (node.val != current) {
+                current = node.val;
+                count = 0;
+            }
+            count++;
+            if (maxCount === count) {
+                result.push(current);  
+            } else if (maxCount < count) {
+                result = [current];
+                maxCount = current;
+            }
+            dfs(node.right);
+        }
+    })(root);
+    return result;
 };
