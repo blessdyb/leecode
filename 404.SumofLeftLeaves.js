cb5b80bbc555308815997aa6f4027333028c4bd1
blessdyb <blessdyb@gmail.com>
2021-03-17 22:23:52 -0700

    Update 404.SumofLeftLeaves.js

--- 404.SumofLeftLeaves.js
+++ 404.SumofLeftLeaves.js
@@ -11,5 +11,11 @@
  * @return {number}
  */
 var sumOfLeftLeaves = function(root) {
-    
+    if (root) {
+        if (root.left && !root.left.left && !root.left.right) {
+            return root.left.val + sumOfLeftLeaves(root.right);
+        }
+        return sumOfLeftLeaves(root.left) + sumOfLeftLeaves(root.right);
+    }
+    return 0;
 };
