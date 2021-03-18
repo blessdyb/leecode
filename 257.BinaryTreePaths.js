b2633912c8e3cf4b1c205498e1877fc5fd63051f
blessdyb <blessdyb@gmail.com>
2021-03-17 22:10:17 -0700

    Update 257.BinaryTreePaths.js

--- 257.BinaryTreePaths.js
+++ 257.BinaryTreePaths.js
@@ -32,17 +32,37 @@ var binaryTreePaths = function(root) {
     const paths = [];
     const stack = [[root, []]];
     while(stack.length) {
-        const [node, ancestor] = stack.shift();
+        const [node, ancestor] = stack.shift(); // Note: BFS we are always get the item from the bottom of the stack, layer by layer
         const updatedAncestor = ancestor.concat(node.val);
         if (!node.left && !node.right) {
             paths.push(updatedAncestor.join('->'));
         }
-        if (node.left) {
+        if (node.left) { // tranverse layer from left to right, layer by layer
             stack.push([node.left, updatedAncestor]);
         }
         if (node.right) {
             stack.push([node.right, updatedAncestor])
         }
+    }
+    return paths;
+};
+
+var binaryTreePaths = function(root) {
+    const paths = [];
+    const stack = [[root, []]];
+    while(stack.length) {
+        const [node, ancestor] = stack.pop(); // Note: DFS we are always get the item from the top of the stack
+        const updatedAncestor = ancestor.concat(node.val);
+        if (!node.left && !node.right) {
+            paths.push(updatedAncestor.join('->'));
+        }
+        if (node.right) { // always push the right node into the stack first for later usage, then push the left node to make sure the left node can get pop out from the top of the stack
+            stack.push([node.right, updatedAncestor]);
+        }
+        if (node.left) {
+            stack.push([node.left, updatedAncestor]);
+        }
+        
     }
     return paths;
 };
