786526427818da76f310563eaf7fcca3c4459c1f
blessdyb <blessdyb@gmail.com>
2021-03-19 00:31:35 -0700

    Update 559.MaximumDepthofN-aryTree.js

--- 559.MaximumDepthofN-aryTree.js
+++ 559.MaximumDepthofN-aryTree.js
@@ -11,5 +11,9 @@
  * @return {number}
  */
 var maxDepth = function(root) {
-    
+    if (root) {
+        const childs = root.children.map(c => maxDepth(c));
+        return 1 + (childs.length ? Math.max(...childs) : 0);
+    }
+    return 0;
 };
