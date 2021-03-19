219e14a022a344c5f87945346ebb094c8ea88b23
blessdyb <blessdyb@gmail.com>
2021-03-18 22:45:03 -0700

    Update 530.MinimumAbsoluteDifferenceinBST.js

--- 530.MinimumAbsoluteDifferenceinBST.js
+++ 530.MinimumAbsoluteDifferenceinBST.js
@@ -10,6 +10,20 @@
  * @param {TreeNode} root
  * @return {number}
  */
+// BST means it's a sorted array when tranversing with DFS, min absolute diff only happens in adjacent elements, so we just need to remember previous node when tranversing
 var getMinimumDifference = function(root) {
-    
+    let min = Infinity;
+    let prev;
+    (function dfs(node){
+        if (node) {
+            dfs(node.left);
+            if (prev) {
+                min = Math.min(min, Math.abs(prev.val - node.val));   
+            }
+            prev = node;
+            dfs(node.right);
+        }
+        return min;
+    })(root);
+    return min;
 };
