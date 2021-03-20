70cddd9f82183be70ca135b17fab8d3af2074ab7
blessdyb <blessdyb@gmail.com>
2021-03-19 17:46:16 -0700

    Create 572.SubtreeofAnotherTree.js

---
+++ 572.SubtreeofAnotherTree.js
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} s
+ * @param {TreeNode} t
+ * @return {boolean}
+ */
+var isSubtree = function(s, t) {
+    if (s.val === t.val) {
+        return isSubtree(s.left, t.left) && isSubtree()
+    }
+};
