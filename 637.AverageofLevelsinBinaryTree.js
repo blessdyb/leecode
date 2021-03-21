3670c04dd90c755b17dcfdf49485955b624ac2f0
blessdyb <blessdyb@gmail.com>
2021-03-20 22:02:46 -0700

    Update 637.AverageofLevelsinBinaryTree.js

--- 637.AverageofLevelsinBinaryTree.js
+++ 637.AverageofLevelsinBinaryTree.js
@@ -11,5 +11,29 @@
  * @return {number[]}
  */
 var averageOfLevels = function(root) {
-    
+    let result = [];
+    let stack = [root];
+    let count = 1;
+    let current = 0;
+    let temp = 0;
+    while(stack.length) {
+        const n = stack.shift();
+        if (n) {
+            current++;
+            temp += n.val;
+            if (n.left) {
+                stack.push(n.left);
+            }
+            if (n.right) {
+                stack.push(n.right);
+            }
+            if (current === count) {
+                result.push(temp / count);
+                temp = 0;
+                count = stack.length;
+                current = 0;
+            }
+        }
+    }
+    return result;
 };
