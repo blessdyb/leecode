1f0a41b1536729753c1b3b5af1be07d6a0bef7a5
blessdyb <blessdyb@gmail.com>
2021-03-20 21:37:57 -0700

    Update 617.MergeTwoBinaryTrees.js

--- 617.MergeTwoBinaryTrees.js
+++ 617.MergeTwoBinaryTrees.js
@@ -12,5 +12,10 @@
  * @return {TreeNode}
  */
 var mergeTrees = function(root1, root2) {
-    
+    if (root1 && root2) {
+        root1.val = root1.val + root2.val;
+        root1.left = mergeTrees(root1.left, root2.left);
+        root1.right = mergeTrees(root1.right, root2.right);
+    }
+    return root1 || root2;
 };
