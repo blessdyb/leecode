ec063a693236c16704cb1c9975cd7e19f7c590df
blessdyb <blessdyb@gmail.com>
2021-03-20 21:24:52 -0700

    Update 606.ConstructStringfromBinaryTree.js

--- 606.ConstructStringfromBinaryTree.js
+++ 606.ConstructStringfromBinaryTree.js
@@ -26,7 +26,9 @@ var tree2str = function(t) {
     }
     return '';
 };
-
+// Since for each node, we need to wrap all it's node into parenthesis. It means we need to start with the parent node and end with the parent node. The traiditional
+// tranverse iteration will only visit the parent node once since we will pop it. To make sure we can visit all nodes twice, we don't pop out the node at the first time,
+// just reference it from the stack top and save it to another visited list. 
 var tree2str = function(t) {
     if (!t) {
         return '';   
