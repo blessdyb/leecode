9402614c47ac4de7f2e4eb8bfa24f95674230d5a
blessdyb <blessdyb@gmail.com>
2021-03-20 21:56:39 -0700

    Update 617.MergeTwoBinaryTrees.js

--- 617.MergeTwoBinaryTrees.js
+++ 617.MergeTwoBinaryTrees.js
@@ -19,3 +19,26 @@ var mergeTrees = function(root1, root2) {
     }
     return root1 || root2;
 };
+
+var mergeTrees = function(root1, root2) {
+    if (root1 && root2) { // Make sure we cover the edge case, since we need to create a new TreeNode if it's null. At the beginning, if root1 is null, we need to catch this case.
+        const stack = [[root1, root2]];
+        while(stack.length) {
+            const [n1, n2] = stack.shift();
+            n1.val += n2;
+            if (n2.left) {
+                if (!n1.left) {
+                    n1.left = new TreeNode(0);   
+                }
+                stack.push([n1.left, n2.left]);
+            }
+            if (n2.right) {
+                if (!n1.right) {
+                    n1.right = new TreeNode(0);   
+                }
+                stack.push([n1.right, n2.right]);
+            }
+        }
+    }
+    return root1 || root2;
+};
