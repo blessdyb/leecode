4adec2e0a56a2de59c3ff0e71e4ae63a3e545d5b
blessdyb <blessdyb@gmail.com>
2021-03-23 09:55:26 -0700

    Update 98.ValidateBinarySearchTree.js

--- 98.ValidateBinarySearchTree.js
+++ 98.ValidateBinarySearchTree.js
@@ -10,6 +10,24 @@
  * @param {TreeNode} root
  * @return {boolean}
  */
-var isValidBST = function(root) {
-    
+var isValidBST = function(root) { // Based on the BST defination, we can get a sorted array while traversing the tree by DFS. So just need to compare the adjacent element.
+    const stack = [];
+    let current = root;
+    let previous;
+    while(true) {
+        if (current) {
+            stack.push(current); 
+            current = current.left;
+        } else if (stack.length) {
+            const node = stack.pop();
+            if (previous && previous.val >= node.val) {
+                return false; 
+            }
+            previous = node;
+            current = node.right;
+        } else {
+            break;
+        }
+    }
+    return true;
 };
