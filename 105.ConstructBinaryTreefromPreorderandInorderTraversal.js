7fa7eda50cd4f5d9de275f233146b2862ae1e4b0
blessdyb <blessdyb@gmail.com>
2021-03-23 22:34:24 -0700

    Update 105.ConstructBinaryTreefromPreorderandInorderTraversal.js

--- 105.ConstructBinaryTreefromPreorderandInorderTraversal.js
+++ 105.ConstructBinaryTreefromPreorderandInorderTraversal.js
@@ -11,7 +11,7 @@
  * @param {number[]} inorder
  * @return {TreeNode}
  */
-var buildTree = function(preorder, inorder) {
+var buildTree = function(preorder, inorder) {  // Just need to use inorder to find the root node, then we can split the given two number list as left and right part. Recursively perform the action until there's no elements left in the number list.
     if (preorder.length) {
         const rootValue = preorder[0];
         const rootIndex = inorder.indexOf(rootValue);
