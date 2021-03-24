65470955aa9744926d288698120d24fccb90ae3a
blessdyb <blessdyb@gmail.com>
2021-03-24 13:56:29 -0700

    Update 1038.BinarySearchTreetoGreaterSumTree.js

--- 1038.BinarySearchTreetoGreaterSumTree.js
+++ 1038.BinarySearchTreetoGreaterSumTree.js
@@ -11,5 +11,30 @@
  * @return {TreeNode}
  */
 var bstToGst = function(root) {
-    
+    let sum = 0;
+    let previous;
+    let current = root;
+    while(current) {
+        if (!current.right) {
+            sum += current.val;
+            current.val = sum;
+            previous = current;
+            current = current.left;
+        } else {
+            previous = current.right;
+            while(previous.left && previous.left !== current) {
+                previous = previous.left;
+            }
+            if (previous.left) {
+                previous.left = null;;
+                sum += current.val;
+                current.val = sum;
+                current = current.left
+            } else {
+                previous.left = current;
+                current = current.right;
+            }
+        }
+    }
+    return root;
 };
