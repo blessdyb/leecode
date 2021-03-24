8cfc20b3a65a9e3e0c73157b3a123f6cee33c4f0
blessdyb <blessdyb@gmail.com>
2021-03-23 22:45:16 -0700

    Update 106.ConstructBinaryTreefromInorderandPostorderTraversal.js

--- 106.ConstructBinaryTreefromInorderandPostorderTraversal.js
+++ 106.ConstructBinaryTreefromInorderandPostorderTraversal.js
@@ -12,5 +12,19 @@
  * @return {TreeNode}
  */
 var buildTree = function(inorder, postorder) {
-    
+    if (inorder.length) {
+        const rootValue = postorder[postorder.length - 1];
+        const rootIndex = inorder.indexOf(rootValue);
+        
+        const leftTreeInOrder = inorder.slice(0, rootIndex);
+        const rightTreeInOrder = inorder.slice(rootIndex + 1);
+        const leftTreePostOrder = postorder.slice(0, leftTreeInOrder.length);
+        const rightTreePostOrder = postorder.slice(leftTreeInOrder.length, postorder.length - 1);
+        
+        const node = new TreeNode(rootValue);
+        node.left = buildTree(leftTreeInOrder, leftTreePostOrder);
+        node.right = buildTree(rightTreeInOrder, rightTreePostOrder);
+        return node;
+    }
+    return null;
 };
