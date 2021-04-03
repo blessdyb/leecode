6f40e57bf41bf1cf0416bd5792cb3ee52b490aac
blessdyb <blessdyb@gmail.com>
2021-04-03 12:53:59 -0700

    Update 919. Complete Binary Tree Inserter.js

--- 919. Complete Binary Tree Inserter.js
+++ 919. Complete Binary Tree Inserter.js
@@ -52,3 +52,51 @@ CBTInserter.prototype.insert = function(v) {  //Brute force since we can always 
 CBTInserter.prototype.get_root = function() {
     return this.nodes[0];
 };
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+
+
+var CBTInserter = function(root) {
+    this.root = root;
+    const stack = [root];
+    this.nodesAcceptChild = [];
+    while(stack.length) {
+        const n = stack.shift();
+        if (n) {
+            if (n.left) {
+                stack.push(n.left);
+            }
+            if (n.right) {
+                stack.push(n.right);
+            }
+            if ((!n.left && !n.right ) || !n.left || !n.right) {
+                this.nodesAcceptChild.unshift(n); 
+            }
+        }
+    }
+};
+CBTInserter.prototype.insert = function(v) { // We can put all nodes which left/right node is null to a queue. So when new node is coming, we just need to inser it to the first node in the queue. Also we push the new node to the queue
+    const node = new TreeNode(v);
+    const topNode = this.nodesAcceptChild[this.nodesAcceptChild.length - 1];
+    if (!topNode.left) {
+        topNode.left = node;
+    } else if (!topNode.right) {
+        topNode.right = node;
+    }
+    this.nodesAcceptChild.unshift(node);
+    if (topNode.left && topNode.right) {
+        this.nodesAcceptChild.pop();
+    }
+    return topNode.val;
+};
+CBTInserter.prototype.get_root = function() {
+    return this.root;
+};
