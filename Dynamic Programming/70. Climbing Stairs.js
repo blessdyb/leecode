32db77cde6cd00b79a8e1a5ef77821997a1e6caf
blessdyb <blessdyb@gmail.com>
2021-04-06 17:22:18 -0700

    Update 70. Climbing Stairs.js

--- Dynamic Programming/70. Climbing Stairs.js
+++ Dynamic Programming/70. Climbing Stairs.js
@@ -9,3 +9,20 @@ var climbStairs = function(n) {
     }
     return dp[n];
 };
+
+
+var climbStairs = function(n) {
+    if (n === 1) {
+        return 1;   
+    } else if (n === 2) {
+        return 2;   
+    } else {
+        let first = 1;     // Since we only care about the result, we can save some space with Fibonacci
+        let second = 2;
+        for(let i = 3; i <=n; i++) {
+            [first, second] = [second, first];
+            second = first + second;
+        }
+        return second;
+    }
+}
