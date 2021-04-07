413479b22e46823a857c53d5a18df8589f02ae3e
blessdyb <blessdyb@gmail.com>
2021-04-06 23:37:29 -0700

    Update 121. Best Time to Buy and Sell Stock.js

--- Dynamic Programming/121. Best Time to Buy and Sell Stock.js
+++ Dynamic Programming/121. Best Time to Buy and Sell Stock.js
@@ -14,3 +14,14 @@ var maxProfit = function(prices) {    // Max profile only happens when we buy at
     }
     return profit;
 };
+
+
+var maxProfit = function(prices) {
+    const dp = [[0, -prices[0]]];    // Base case, 0 day [no profile if we do nothing, negative price if we just buy].
+    for (let i = 1; i < prices.length; i++) {
+        dp[i] = [];
+        dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+        dp[i][1] = Math.max(dp[i - 1][1], -prices[i]);
+    }
+    return dp[prices.length - 1][0];   // Result we just need to check when we sell stock, if we keep stock in hand, the profit is negative.
+}
