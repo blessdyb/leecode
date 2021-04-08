4e9f9f443c04b11e5dfe01d33c3cd8b86c5a6fd1
blessdyb <blessdyb@gmail.com>
2021-04-08 09:21:53 -0700

    Create 338. Counting Bits.js

---
+++ Dynamic Programming/338. Counting Bits.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} num
+ * @return {number[]}
+ */
+var countBits = function(num) {
+    const dp = [0];
+    for(let i = 1; i <= num; i++) {
+        if (i % 2 === 1) {
+            dp[i] = dp[i - 1] + 1;
+        } else {
+            dp[i] = dp[Math.floor(i / 2)];
+        }
+    }
+    return dp;
+};
