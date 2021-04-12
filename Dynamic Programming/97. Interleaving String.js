f734dee65b36e5ca3a81d5b3e96413d13dcbee0d
blessdyb <blessdyb@gmail.com>
2021-04-12 10:14:23 -0700

    Update 97. Interleaving String.js

--- Dynamic Programming/97. Interleaving String.js
+++ Dynamic Programming/97. Interleaving String.js
@@ -43,3 +43,24 @@ var isInterleave = function(s1, s2, s3) {   // DP
     return dp[s1.length][s2.length];
 };
 
+var isInterleave = function(s1, s2, s3) {  // Based on the state transform equation, dp[i][j] relys on dp[i - 1][j] and dp[i][j - 1], so we can compress the space usage to 1D array
+    if (s1.length + s2.length !== s3.length) {
+        return false;   
+    }
+    const dp = [];
+    for (let i = 0; i <= s1.length; i++) {
+        for(let j = 0; j <= s2.length; j++) {
+            if (i === 0 && j === 0) {
+                dp[j] = true;   
+            } else if (i === 0) {
+                dp[j] = dp[j - 1] && s2[j - 1] === s3[i + j - 1];   
+            } else if (j === 0) {
+                dp[j] = dp[j] && s1[i - 1] === s3[i + j - 1];   
+            } else {
+                dp[j] = (dp[j - 1] && s2[j - 1] === s3[i + j - 1]) ||  (dp[j] && s1[i - 1] === s3[i + j - 1])l
+            }
+        }
+    }
+    return dp[s2.length];
+    
+}
