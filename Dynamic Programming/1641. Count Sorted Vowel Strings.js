6642d1127f6c4708bd1eb7f26f0abdcecfef40dd
blessdyb <blessdyb@gmail.com>
2021-04-13 23:59:57 -0700

    Update 1641. Count Sorted Vowel Strings.js

--- Dynamic Programming/1641. Count Sorted Vowel Strings.js
+++ Dynamic Programming/1641. Count Sorted Vowel Strings.js
@@ -11,6 +11,32 @@ var countVowelStrings = function(n) {  // Recursive
             }
             return count;
         }
-        return 1; // Base case, given prefix character, the total combination is 1
+        return 1; //  base case when n === 0, it means we have assigned the string, so the value is 1.
     })(0, n);
 };
+
+var countVowelStrings = function(n) {  // DP  dp[i][j] = Sum(dp[i - 1][?]) = dp[i - 1][j] + dp[i][j + 1]
+    const dp = [[1, 1, 1, 1, 1]];
+    for(let i = 1; i <=n; i++) {
+        dp[i] = [];
+        dp[i][4] = 1;  // given n length you can only have one combination of string with all characters as u
+        for (let j = 3; j >= 0; j--) {
+            dp[i][j] = dp[i - 1][j] + dp[i][j + 1];   
+        }
+    }
+    return dp[n][0];
+}
+
+var countVowelStrings = function(n) {
+    let [a, e, i, o, u] = [1, 1, 1, 1, 1];
+    while(n - 1 > 0) {
+        u = u;
+        o += u;
+        i += o;
+        e += i;
+        a += e;
+        n--;
+    }
+    return a + e + i + o + u;
+};
+
