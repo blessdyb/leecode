c498e249f09e41e9fbd6f6ee443012668d1a439a
blessdyb <blessdyb@gmail.com>
2021-04-18 00:31:05 -0700

    Update 1140. Stone Game II.js

--- Dynamic Programming/1140. Stone Game II.js
+++ Dynamic Programming/1140. Stone Game II.js
@@ -28,3 +28,28 @@ var stoneGameII = function(piles) {   // Cache + Recursive
     }
     return (total + helper(0, 1)) / 2;
 };
+
+var stoneGameII = function(piles) {
+    const n = piles.length;
+    const preSum = [];
+    const cache = {};
+    preSum[n - 1] = piles[n - 1];
+    for(let i = n - 2; i >= 0; i--) {
+        preSum[i] = preSum[i + 1] + piles[i];   
+    }
+    return (function helper(s, m) {   // Use preSum[i] - opponent's min score we can get players max score. 
+        if (s + 2 * m >= n) {
+            return preSum(s);   
+        }
+        const key = `${s}_${m}`;
+        if (cache[key] === undefined) {
+            let opponent = Infinity;
+            for(let i = 1; i <= 2 * m; i++) {
+                opponent = Math.min(opponent, helper(s + i, Math.max(i, m)));   
+            }
+            cache[key] = preSum[i] - opponent;
+        }
+        return cache[key];
+    })(0, 1);
+};
+
