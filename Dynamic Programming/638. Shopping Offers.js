5559ab85d45b79518e29231bcfb7ada0c6486665
blessdyb <blessdyb@gmail.com>
2021-05-08 12:00:42 -0700

    Create 638. Shopping Offers.js

---
+++ Dynamic Programming/638. Shopping Offers.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} price
+ * @param {number[][]} special
+ * @param {number[]} needs
+ * @return {number}
+ */
+var shoppingOffers = function(price, special, needs) {
+    const sum = needs.reduce((acc, item) => acc + item, 0);
+    if(sum === 0) {
+        return 0; 
+    }
+    let minPrice = Infinity;
+    for(let i = 0; i < special.length; i++) {
+        let isSpecialOfferValid = true;
+        const newNeeds = [];
+        for(let j = 0; j < needs.length; j++) {
+            if (needs[j] < special[i][j]) {
+                isSpecialOfferValid = false;
+            } else {            
+                newNeeds.push(needs[j] - special[i][j]);
+            }
+        }
+        if(isSpecialOfferValid) {
+            minPrice = Math.min(minPrice, shoppingOffers(price, special, newNeeds));
+        }
+    }
+    minPrice = Math.min(minPrice, needs.reduce((acc, item, index) => acc + item * price[index], 0));
+    return minPrice;
+};
