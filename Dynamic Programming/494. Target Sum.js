1f13bd574adfeb71d2ec36da1f4a016606350891
blessdyb <blessdyb@gmail.com>
2021-05-09 21:10:51 -0700

    Update 494. Target Sum.js

--- Dynamic Programming/494. Target Sum.js
+++ Dynamic Programming/494. Target Sum.js
@@ -22,3 +22,21 @@ var findTargetSumWays = function(nums, target) {
     helper(n - 1, target);
     return result;
 };
+
+var findTargetSumWays = function(nums, target) {
+    const n = nums.length;
+    const sum = nums.reduce((acc, item) => acc + item, 0);
+    const dp = new Array(n + 1).fill(0).map(item => new Array(2 * sum + 1).fill(0));
+    dp[0][sum] = 1; // given target 0, with 0 nums, we have 1 way
+    for(let i = 1; i <= n; i++) {
+        for(let j = -sum; j <= sum; j++) {
+            if (j + nums[i - 1] <= sum) {
+                dp[i][j + sum + nums[i - 1]] += dp[i - 1][j + sum];   
+            }
+            if (j - nums[i - 1] >= -sum) {
+                dp[i][j + sum - nums[i - 1]] += dp[i - 1][j + sum];   
+            }
+        }
+    }
+    return target > sum || target < -sum ? 0 : dp[n][target + sum];
+};
