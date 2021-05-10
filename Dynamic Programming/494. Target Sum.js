7a70d54eb7438d78e11b8e3e36a92ee617b1ecf8
blessdyb <blessdyb@gmail.com>
2021-05-09 21:17:19 -0700

    Update 494. Target Sum.js

--- Dynamic Programming/494. Target Sum.js
+++ Dynamic Programming/494. Target Sum.js
@@ -30,6 +30,7 @@ var findTargetSumWays = function(nums, target) {
     dp[0][sum] = 1; // given target 0, with 0 nums, we have 1 way
     for(let i = 1; i <= n; i++) {
         for(let j = -sum; j <= sum; j++) {
+            // State transform function:  dp[i][j + nums[i]] += dp[i][j] and dp[i][j - nums[i]] += dp[i][j] (consider it like a binary tree)
             if (j + nums[i - 1] <= sum) {
                 dp[i][j + sum + nums[i - 1]] += dp[i - 1][j + sum];   
             }
@@ -39,4 +40,25 @@ var findTargetSumWays = function(nums, target) {
         }
     }
     return target > sum || target < -sum ? 0 : dp[n][target + sum];
+};
+
+// Based on the state transform function, we can use a 1D DP
+var findTargetSumWays = function(nums, target) {
+    const n = nums.length;
+    const sum = nums.reduce((acc, item) => acc + item, 0);
+    let dp = new Array(2 * sum + 1).fill(0);
+    dp[sum] = 1;
+    for(let i = 1; i <= n; i++) {
+        const temp = new Array(2 * sum + 1).fill(0);
+        for(let j = -sum; j <= sum; j++) {
+            if (j + nums[i - 1] <= sum) {
+                temp[j + sum + nums[i - 1]] += dp[j + sum];   
+            }
+            if (j - nums[i - 1] >= -sum) {
+                temp[j + sum - nums[i - 1]] += dp[j + sum];   
+            }
+        }
+        dp = temp;
+    }
+    return target > sum || target < -sum ? 0 : dp[target + sum];
 };
