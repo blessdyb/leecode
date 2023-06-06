a171ae1745ab1219139b995df3e0ee38a196d352
blessdyb <blessdyb@gmail.com>
2023-06-05 22:48:37 -0700

    update (#1)

--- Golang/BinarySearch/69.sqrt(x).go
+++ Golang/BinarySearch/69.sqrt(x).go
@@ -1,15 +1,17 @@
-/** Given any integer, it's a sorted digits 1 ~ n and we want to find sqrt(x) which is a perfect case for the binary search
- */
+package main
+
+// Given 0 ~ 2^31 - 1 range sorted array, to find a target sqrt(x), the only solution is binary search
+// To find the nearst value, we need to remember the smaller value before the next binary search round in case there's no exact match
 func mySqrt(x int) int {
 	i, j := 0, x
 	ret := 0
 	for i <= j {
-		h := i + (j - i) / 2
-		h2 = h * h
-		if h2 === x {
+		h := i + (j-i)/2
+		sqrt := h * h
+		if sqrt == x {
+			return h
+		} else if sqrt < x {
 			ret = h
-			break
-		} else if h2 < x {
 			i = h + 1
 		} else {
 			j = h - 1
