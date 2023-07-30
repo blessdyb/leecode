356301841b4c4dabeff2263e0b4a52a793e98cdc
blessdyb <blessdyb@gmail.com>
2023-07-30 00:00:40 -0700

    WIP

---
+++ Golang/Tree/951.Flip-equivalent-binary-tree.go
@@ -0,0 +1,16 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func flipEquiv(root1 *TreeNode, root2 *TreeNode) bool {
+	if root1 != nil && root2 != nil && root1.Val == root2.Val {
+		return (flipEquiv(root1.Left, root2.Left) && flipEquiv(root1.Right, root2.Right)) || (flipEquiv(root1.Right, root2.Left) && flipEquiv(root1.Left, root2.Right))
+	} else if root1 == nil && root2 == nil {
+		return true
+	}
+	return false
+}
