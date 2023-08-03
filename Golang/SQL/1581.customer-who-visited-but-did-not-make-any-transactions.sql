2e57e2a7b447f9af8c819d8995f6ad1e22809814
blessdyb <blessdyb@gmail.com>
2023-08-02 22:04:04 -0700

    WIP

---
+++ Golang/SQL/1581.customer-who-visited-but-did-not-make-any-transactions.sql
@@ -0,0 +1,5 @@
+# NOT IN Solution
+select customer_id, count(customer_id) as count_no_trans from Visits where visit_id not in (select visit_id from Transactions) group by customer_id 
+
+# LEFT JOIN Solution
+select v.customer_id, count(v.customer_id) as count_no_trans from Visits v left join Transactions t on v.visit_id = t.visit_id where t.transaction_id is null group by v.customer_id
