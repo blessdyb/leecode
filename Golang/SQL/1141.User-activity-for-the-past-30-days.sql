4b67cf1e9eca16819b47ac6a24c75418ed1e86ba
blessdyb <blessdyb@gmail.com>
2023-08-08 00:01:05 -0700

    WIP

--- Golang/SQL/1141.User-activity-for-the-past-30-days.sql
+++ Golang/SQL/1141.User-activity-for-the-past-30-days.sql
@@ -1,2 +1,2 @@
-# Write your MySQL query statement below
+# DATEDIFF /  COUNT(DISTINCT  ) 
 select activity_date as day, count(distinct user_id) as active_users from Activity where activity_date <= "2019-07-27" and DATEDIFF('2019-07-27', activity_date)<30  group by activity_date
