e3f0e559b2ec8617d8b79a1e48aa949a33ce7112
blessdyb <blessdyb@gmail.com>
2023-08-13 13:03:33 -0700

    WIP

---
+++ Golang/LinkedList/1290.convert-binary-number-in-a-linked-list-to-integer.go
@@ -0,0 +1,15 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func getDecimalValue(head *ListNode) int {
+	ret := 0
+	for head != nil {
+		ret = ret*2 + head.Val
+		head = head.Next
+	}
+	return ret
+}
