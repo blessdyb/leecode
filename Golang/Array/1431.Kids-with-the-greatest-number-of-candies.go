9b0bc1d25c61ccfc9c8de5cd95d14de277edb01b
blessdyb <blessdyb@gmail.com>
2023-08-19 09:27:22 -0700

    WIP

---
+++ Golang/Array/1431.Kids-with-the-greatest-number-of-candies.go
@@ -0,0 +1,17 @@
+package main
+
+func kidsWithCandies(candies []int, extraCandies int) []bool {
+	max := candies[0]
+	for i := 1; i < len(candies); i++ {
+		if max < candies[i] {
+			max = candies[i]
+		}
+	}
+	ret := make([]bool, len(candies))
+	for i := 0; i < len(candies); i++ {
+		if candies[i]+extraCandies >= max {
+			ret[i] = true
+		}
+	}
+	return ret
+}
