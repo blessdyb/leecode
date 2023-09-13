4ed5767ff203269dd3c6f4b85fc0014d294e1b59
blessdyb <blessdyb@gmail.com>
2023-09-12 23:55:52 -0700

    WIP

---
+++ Golang/String/412.Fizz-buzz.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func fizzBuzz(n int) []string {
+	ret := []string{}
+	for i := 1; i <= n; i++ {
+		if i%3 == 0 && i%5 == 0 {
+			ret = append(ret, "FizzBuzz")
+		} else if i%3 == 0 {
+			ret = append(ret, "Fizz")
+		} else if i%5 == 0 {
+			ret = append(ret, "Buzz")
+		} else {
+			ret = append(ret, fmt.Sprint(i))
+		}
+	}
+	return ret
+}
