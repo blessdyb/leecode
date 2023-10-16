781b9fe73cdc1f43158d732f3ac3e0337a3b2111
blessdyb <blessdyb@gmail.com>
2023-10-16 16:23:53 -0700

    WIP

---
+++ Golang/Math/883.projection-area-of-3d-shapes.go
@@ -0,0 +1,25 @@
+package main
+
+func projectionArea(grid [][]int) int {
+	n := len(grid)
+	top, front, side := 0, 0, 0
+	max := func(a, b int) int {
+		if a > b {
+			return a
+		}
+		return b
+	}
+	for i := 0; i < n; i++ {
+		row, col := 0, 0
+		for j := 0; j < n; j++ {
+			if grid[i][j] > 0 {
+				top++
+			}
+			row = max(row, grid[i][j])
+			col = max(row, grid[j][i])
+		}
+		front += row
+		side += col
+	}
+	return top + front + side
+}
