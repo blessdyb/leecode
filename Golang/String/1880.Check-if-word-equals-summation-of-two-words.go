57e1b8f8bf4ab777cbe9dda6f0ea6be0a0694ee2
blessdyb <blessdyb@gmail.com>
2023-10-19 21:48:11 -0700

    WIP

---
+++ Golang/String/1880.Check-if-word-equals-summation-of-two-words.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"strconv"
+	"strings"
+)
+
+func isSumEqual(firstWord string, secondWord string, targetWord string) bool {
+	getValue := func(str string) int {
+		digits := []string{}
+		for i := 0; i < len(str); i++ {
+			digits = append(digits, strconv.Itoa(int(str[i]-'a')))
+		}
+		digitsStr := strings.Join(digits, "")
+		ret := 0
+		for i, j := len(digitsStr)-1, 1; i >= 0; {
+			ret += int(digitsStr[i]-'0') * j
+			i--
+			j *= 10
+		}
+		return ret
+	}
+	return getValue(firstWord)+getValue(secondWord) == getValue(targetWord)
+}
