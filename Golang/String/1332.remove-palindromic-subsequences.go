33f5af7f1e19255acbe06e869263a409c1f0412c
blessdyb <blessdyb@gmail.com>
2023-10-31 23:49:29 -0700

    WIP

---
+++ Golang/String/1332.remove-palindromic-subsequences.go
@@ -0,0 +1,14 @@
+package main
+
+func removePalindromeSub(s string) int {
+	if s == "" {
+		return 0
+	}
+	for i, j := 0, len(s)-1; i < j; i++ {
+		if s[i] != s[j] {
+			// If the given string is not a palindrome, you can remove all the 'a's and 'b's from it separately. This is because all 'a's can be removed in one step, and all 'b's can be removed in one step. In the end, you'll have an empty string.
+			return 2
+		}
+	}
+	return 1
+}
