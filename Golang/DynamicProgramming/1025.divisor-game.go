394187a73e39891bc577fee3066216bbb29abfaa
blessdyb <blessdyb@gmail.com>
2023-11-01 21:10:11 -0700

    WIP

---
+++ Golang/DynamicProgramming/1025.divisor-game.go
@@ -0,0 +1,16 @@
+package main
+
+func divisorGame(n int) bool {
+	dp := make([]bool, n+1)
+	dp[0] = false
+	dp[1] = false
+	for i := 2; i <= n; i++ {
+		for j := 1; j < i; j++ {
+			if i%j == 0 && !dp[i-j] {
+				dp[i] = true
+				break
+			}
+		}
+	}
+	return dp[n]
+}
